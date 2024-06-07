04306d04b6ca5a0cee689eb20cefd9c154dd30e0
Bernardo Andrade <bernardo.andrade@engenharia.ufjf.br>
2024-06-07 12:42:35 -0300

    Adicionando código final do Trabalho 4

--- Trabalhos/Trabalho 4/a_star_functions.py
+++ Trabalhos/Trabalho 4/a_star_functions.py
@@ -1,9 +1,79 @@
 import numpy as np
 import heapq
+import math
 import matplotlib.pyplot as plt
 
 def distancia(ponto1, ponto2):
     return np.sqrt((ponto1[0] - ponto2[0])**2 + (ponto1[1] - ponto2[1])**2)
+
+def ajusta_angulo(angulo):
+    angulo = angulo % (2 * math.pi)
+    if angulo > math.pi:
+        angulo  = angulo - 2 * math.pi
+    return angulo
+
+def simula_processo(objetivos,ganhos_controlador,posicaoInicial,
+                    toleranciaDistancia = 0.1, vmax = 0.5, 
+                    wmax = np.deg2rad(180), dt = 0.1):
+    
+    Kp, Ki, Kd = ganhos_controlador
+
+    x0,y0 = posicaoInicial
+    xg, yg = objetivos[0]
+    ex=[x0,y0,0.0]
+    posicao = np.array(ex,float)
+    historico_posicao = [(x0,y0,0.0)]
+    x, y = x0,y0
+    theta = 0
+
+    integral_alpha = 0
+    derivada_alpha = 0
+    alpha_anterior = 0
+    tempo_corrente = 0
+    tempo_maximo = 10* math.sqrt((xg - x0)**2 + (yg - y0)**2)/vmax
+
+    erroX = xg - x
+    erroY = yg - y
+    rho = math.sqrt(erroX**2 + erroY**2)
+    gamma = ajusta_angulo(math.atan2(erroY,erroX))
+    alpha = ajusta_angulo(gamma - theta)
+
+    for objetivo in objetivos:
+        xg, yg = objetivo
+        tempo_corrente = 0
+        tempo_maximo = 10* math.sqrt((xg - x0)**2 + (yg - y0)**2)/vmax
+
+        erroX = xg - x
+        erroY = yg - y
+
+        rho = math.sqrt(erroX**2 + erroY**2)
+        gamma = ajusta_angulo(math.atan2(erroY,erroX))
+        alpha = ajusta_angulo(gamma - theta)
+        while rho > toleranciaDistancia and tempo_corrente <= tempo_maximo:
+            tempo_corrente += dt
+            erroX = xg - x
+            erroY = yg - y
+
+            rho = math.sqrt(erroX**2 + erroY**2)
+            gamma = ajusta_angulo(math.atan2(erroY,erroX))
+            alpha = ajusta_angulo(gamma - theta)
+    
+            derivada_alpha = (alpha - alpha_anterior)
+            alpha_anterior = alpha
+            integral_alpha += alpha
+
+            v = vmax
+            w = Kp * alpha + Ki * integral_alpha + Kd * derivada_alpha
+            w = np.sign(w) * min(abs(w), wmax)
+
+            variacaoVelocidade = [v*math.cos(theta), v*math.sin(theta), w]
+            deslocamento = np.array(variacaoVelocidade) * dt
+            posicao = np.array(posicao) + deslocamento
+            posicao[2] = ajusta_angulo(posicao[2])
+            historico_posicao.append(posicao)
+            x, y, theta = posicao
+
+    return  historico_posicao
 
 def a_estrela(mapa, inicio, objetivo,conectividade):
     linhas, colunas = mapa.shape
@@ -37,40 +107,95 @@ def a_estrela(mapa, inicio, objetivo,conectividade):
                     custo_f[vizinho] = tentativa_g + distancia(vizinho, objetivo)
                     heapq.heappush(lista_aberta, (custo_f[vizinho], vizinho))
     
-    return None  # Se nenhum caminho for encontrado
+    return [], float('inf')  # Se nenhum caminho for encontrado
 
-def gerar_mapa(tamanho, obstaculos=[],start_index = 1):
-    mapa = np.zeros((tamanho+1, tamanho+1))
+def gerar_mapa(tamanho, obstaculos=[]):
+    mapa = np.zeros((tamanho + 2, tamanho + 2))
+  
     # Adiciona obstáculos personalizados
     for obstaculo in obstaculos:
         x, y = obstaculo
         mapa[x, y] = -1
+        
+    # Adiciona a margem
+    mapa[0, :] = -1
+    mapa[-1, :] = -1
+    mapa[:, 0] = -1
+    mapa[:, -1] = -1
+
     return mapa
 
-def plotar_mapa(mapa, inicio, objetivo, caminho=None,custo_caminho=None, nomeFigura = 'mapa.png'):
-    fig, ax = plt.subplots()
-    ax.imshow(mapa, cmap='gray')
 
-    ax.plot(inicio[1], inicio[0], 'go', markersize=10)  # Ponto inicial verde
-    ax.plot(objetivo[1], objetivo[0], 'ko', markersize=10)  # Ponto objetivo preto
+def plotar_mapa(mapa, inicio, objetivo, trajeto, caminho=None, custo_caminho=None, nomeFigura='mapa.png'):
+    fig, ax = plt.subplots(figsize=(20, 20))
+    
+    # Destaca a margem com cores diferentes
+    mapa_com_margem = np.copy(mapa)
+    mapa_com_margem[0, :] = 0.5
+    mapa_com_margem[-1, :] = 0.5
+    mapa_com_margem[:, 0] = 0.5
+    mapa_com_margem[:, -1] = 0.5
+    
+    #Plota mapa de acordo com gradiente de cor escolhido, no caso o "cividis"
+    ax.imshow(mapa_com_margem, cmap='cividis', origin='lower')
+
+    # Marcadores maiores
+    ax.plot(inicio[1], inicio[0], 'go', markersize=20)  # Ponto inicial verde
+    ax.plot(objetivo[1], objetivo[0], 'ko', markersize=20)  # Ponto objetivo preto
 
     if caminho:
+        x = [ponto[1] for ponto in caminho]
+        y = [ponto[0] for ponto in caminho]
+        #Plota caminho achado entre os pontos
+        #ax.plot(x, y, linewidth=3)  
         for x, y in caminho:
-            ax.plot(y, x, 'b.', markersize=8)  # Caminho em azul
+            ax.plot(y, x, 'b.', markersize=15)  
 
     for i in range(mapa.shape[0]):
         for j in range(mapa.shape[1]):
             if mapa[i, j] == -1:
-                ax.text(j, i, '*', ha='center', va='center', color='red')  # Obstáculo como asterisco
+                ax.text(j, i, '*', ha='center', va='center', color='red', fontsize=15)
 
-    ax.set_xticks(np.arange(-0.5, mapa.shape[1], 1), minor=True)
-    ax.set_yticks(np.arange(-0.5, mapa.shape[0], 1), minor=True)
+    # Configurando ticks e grid
+    tamanho = mapa.shape[0] - 2
+    ax.set_xticks(np.arange(0, tamanho + 2, 1))
+    ax.set_yticks(np.arange(0, tamanho + 2, 1))
+    ax.set_xticks(np.arange(-0.5, tamanho + 1.5, 1), minor=True)
+    ax.set_yticks(np.arange(-0.5, tamanho + 1.5, 1), minor=True)
     ax.grid(which='minor', color='k', linestyle='-', linewidth=2)
-    ax.set_xticks([])
-    ax.set_yticks([])
+
+    # Configurando rótulos dos ticks para serem de 1 a tamanho
+    ax.set_xticklabels(np.arange(0, tamanho + 2, 1), fontsize=18)
+    ax.set_yticklabels(np.arange(0, tamanho + 2, 1), fontsize=18)
+
+    ax.set_xlabel('X', fontsize=24)
+    ax.set_ylabel('Y', fontsize=24)
     ax.set_aspect('equal')
 
     if custo_caminho:
-        ax.set_title(f'Custo total do caminho: {custo_caminho:.4f}')
+        ax.set_title(f'Custo total do caminho: {custo_caminho:.4f}', fontsize=28, fontweight='bold')
+    else:
+        ax.set_title('Nenhum caminho encontrado', fontsize=28, fontweight='bold')
+
+    if trajeto is not None:
+        x_trajeto = [ponto[1] for ponto in trajeto]
+        y_trajeto = [ponto[0] for ponto in trajeto]
+        theta_trajeto = [ponto[2] for ponto in trajeto]
+
+        ax.plot(x_trajeto, y_trajeto, color='green', linewidth=2)
+
+        ax.plot(x_trajeto[0], y_trajeto[0], 'ko', markersize=15, fillstyle='none')
+        ax.plot(x_trajeto[-1], y_trajeto[-1], 'ro', markersize=15, fillstyle='none')
+
+    # Para transformar em um mapa em centímetros onde cada quadrado tem 100cm (meio do quadrado tem 50cm)
+    # Descomente as linhas abaixo:
+    cm_ticks = np.arange(-50, (tamanho + 2) * 100 - 50, 100)
+    ax.set_xticks(np.arange(0, tamanho + 2, 1))
+    ax.set_yticks(np.arange(0, tamanho + 2, 1))
+    ax.set_xticklabels(cm_ticks, fontsize=13)
+    ax.set_yticklabels(cm_ticks, fontsize=13)
+    ax.set_xlabel('X (cm)', fontsize=24)
+    ax.set_ylabel('Y (cm)', fontsize=24)
 
     plt.savefig(nomeFigura)
+
--- Trabalhos/Trabalho 4/simulation.py
+++ Trabalhos/Trabalho 4/simulation.py
@@ -5,29 +5,44 @@ CONNECTIVITY_4 = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 CONNECTIVITY_8 = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
 
 # Tamanho do mapa
-tamanho = 10
+tamanho = 30
 
 # Definir pontos inicial e objetivo
 inicio = (7,4)
 objetivo = (9,9)
 
 # Definir obstáculos personalizados (opcional)
-obstaculos_personalizados = [(4,1),(9,1),(6,2),(9,2),(1,4),(4,4),(5,4),(6,4),(7,5),(3,6),(7,6),(3,7),(7,7),(9,5),(8,5),(10,5)]
+#obstaculos_personalizados = [(4,1),(9,1),(6,2),(9,2),(1,4),(4,4),(5,4),(6,4),(7,5),(3,6),(7,6),(3,7),(7,7),(9,5),(8,5),(10,5),(6,5)]
+
+# Obstáculo não convexo
+obstaculos_personalizados = [(3,5),(4,3),(4,6),(1,4),(4,4),(4,5),(5,6),(7,6),(7,7),(8,3),(8,4),(8,5),(8,6),(10,5),(6,6),(9,3),(9,7)]
+
+# Obstáculo impossível
+#obstaculos_personalizados = [(5,1),(5,2),(5,3),(5,4),(5,5),(5,6),(5,7),(5,8),(5,9),(5,10)]
+
+# Obstáculos 30 X 30
+#obstaculos_personalizados = [(15,10),(15,11),(15,12),(15,13),(15,14),(15,15),(15,16),(15,17),(15,18),(15,19),(15,20)]
+
 
 # Gerar mapa
 mapa_inicial = gerar_mapa(tamanho, obstaculos_personalizados)
 
 # Executar o algoritmo A*
-caminho,custo_caminho = a_estrela(mapa_inicial, inicio, objetivo,CONNECTIVITY_8)
-
-# Plotar mapa inicial
-plotar_mapa(mapa_inicial, inicio, objetivo, nomeFigura='mapainicial.png')
+caminho,custo_caminho = a_estrela(mapa_inicial, inicio, objetivo,CONNECTIVITY_4)
 
 # Plotar mapa final com o caminho encontrado
 if caminho:
+    # Gerar trajeto com o controlador
+    controlador = [10,0.5,3]
+    trajeto = simula_processo(caminho,controlador,caminho[0])
+
+    #Plotar mapa inicial
+    #plotar_mapa(mapa_inicial, inicio, objetivo ,trajeto=None, nomeFigura='mapainicial.png')
+
     mapa_final = mapa_inicial.copy()
     for passo in caminho:
         mapa_final[passo] = 3
-    plotar_mapa(mapa_final, inicio, objetivo, caminho,custo_caminho, nomeFigura= 'mapaFinal.png')
+    plotar_mapa(mapa_final, inicio, objetivo, trajeto, caminho, custo_caminho, nomeFigura= 'testeFinal30x30.png')
 else:
     print("\nNenhum caminho encontrado.")
+    plotar_mapa(mapa_inicial, inicio, objetivo,trajeto=None, nomeFigura='30x30Teste.png')
